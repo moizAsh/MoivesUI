1e586b51256573a51b8fd9b1a4bc02de6801c095
Moiz <donwebrockin@hotmail.com>
2022-05-17 16:59:18 +0100

    Actor form

--- src/actors/CreateActor.tsx
+++ src/actors/CreateActor.tsx
@@ -1,9 +1,11 @@
+import ActionForm from "./ActorForm";
+
 export default function CreateActor() {
     return (
         <>
-            <h3>
-                Create Actor
-            </h3>
+            <h3>Create Actor</h3>
+            <ActionForm model={{name: '', dateOfBirth: undefined}}
+            onSubmit={values => console.log(values)}/>
         </>
     )
 }
--- src/actors/EditActor.tsx
+++ src/actors/EditActor.tsx
@@ -1,9 +1,13 @@
+import { date } from "yup";
+import ActionForm from "./ActorForm";
+
 export default function EditActor() {
     return (
         <>
-            <h3>
-                Edit Actor
-            </h3>
+            <h3>Edit Actor</h3>
+            <h3>Create Actor</h3>
+            <ActionForm model={{name: 'Tom holland', dateOfBirth: new Date('1996-06-01T00:00:00')}}
+            onSubmit={values => console.log(values)}/>
         </>
     )
 }
