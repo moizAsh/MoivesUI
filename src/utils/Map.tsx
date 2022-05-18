4e4f37ec64e59633953513676dc61e189ac6522f
Moiz <donwebrockin@hotmail.com>
2022-05-18 11:23:28 +0100

    Actor added map and pointer to movie theater

--- src/utils/Map.tsx
+++ src/utils/Map.tsx
@@ -1,8 +1,10 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, useMapEvent, Marker } from "react-leaflet";
 import L from 'leaflet'
 import icon from 'leaflet/dist/images/marker-icon.png'
 import iconShadow from 'leaflet/dist/images/marker-shadow.png'
 import 'leaflet/dist/leaflet.css'
+import coordinateDTO from "./coordinates.model";
+import { useState } from "react";
 let defaultIcon = L.icon({
     iconUrl: icon,
     shadowUrl: iconShadow,
@@ -12,6 +14,7 @@ let defaultIcon = L.icon({
 L.Marker.prototype.options.icon = defaultIcon;
 
 export default function Map(props: mapProps){
+    const [coordinates, setCoordinates] = useState<coordinateDTO[]>([])
     return (
         <MapContainer
             center={[18.482214, -69.914311]} zoom={14}
@@ -20,7 +23,13 @@ export default function Map(props: mapProps){
              <TileLayer attribution="React Movies"
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            <MapClick setCoordinates={coordinates => {
+                setCoordinates([coordinates])
 
+            }}/>
+            {coordinates.map(coordinates => 
+            <Marker
+             position={[coordinates.lat,coordinates.lng]}/> )}
           
         </MapContainer>
     )
@@ -33,3 +42,18 @@ interface mapProps{
 Map.defaultProps = {
     height: '800px'
 }
+
+const MapClick = (props: mapClickProps) => {
+    useMapEvent('click', eventArgs => {
+        props.setCoordinates({lat: eventArgs.latlng.lat, lng: eventArgs.latlng.lng})
+    })
+
+    return null;
+
+}
+
+interface mapClickProps{
+    setCoordinates(coordinates: coordinateDTO ): void
+}
+
+
